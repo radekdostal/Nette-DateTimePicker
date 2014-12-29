b45f699ac9c7d2babf6037c9fdc05325e9289883
Petr 'PePa' Pavel <petr.pavel@pepa.info>
2014-12-29 10:50:12 +0100

    $label should be optional

--- examples/DateTimePicker.php
+++ examples/DateTimePicker.php
@@ -18,7 +18,7 @@ require '../vendor/autoload.php';
 Debugger::$strictMode = TRUE;
 Debugger::enable();
 
-Form::extensionMethod('addDateTimePicker', function(Form $_this, $name, $label, $cols = NULL, $maxLength = NULL)
+Form::extensionMethod('addDateTimePicker', function(Form $_this, $name, $label = NULL, $cols = NULL, $maxLength = NULL)
 {
   return $_this[$name] = new RadekDostal\NetteComponents\DateTimePicker\DateTimePicker($label, $cols, $maxLength);
 });
