dc34b3c27b5df1eb854029f8969aa9ee82192682
Petr 'PePa' Pavel <petr.pavel@pepa.info>
2014-12-29 10:51:02 +0100

    $label should be optional

--- examples/TbDatePicker.php
+++ examples/TbDatePicker.php
@@ -19,7 +19,7 @@ require '../vendor/autoload.php';
 Debugger::$strictMode = TRUE;
 Debugger::enable();
 
-Form::extensionMethod('addTbDatePicker', function(Form $_this, $name, $label, $cols = NULL, $maxLength = NULL)
+Form::extensionMethod('addTbDatePicker', function(Form $_this, $name, $label = NULL, $cols = NULL, $maxLength = NULL)
 {
   return $_this[$name] = new RadekDostal\NetteComponents\DateTimePicker\TbDatePicker($label, $cols, $maxLength);
 });
