9411b504e13d1c4c83f823350fe20f6f698d0da9
Radek Dostál <radek.dostal@gmail.com>
2017-04-03 21:29:37 +0200

    Released version 2.5.1.

--- src/DateTimePicker/DateTimePicker.php
+++ src/DateTimePicker/DateTimePicker.php
@@ -5,7 +5,7 @@
  * @package   RadekDostal\NetteComponents\DateTimePicker
  * @example   https://componette.com/radekdostal/nette-datetimepicker/
  * @author    Ing. Radek Dostál, Ph.D. <radek.dostal@gmail.com>
- * @copyright Copyright (c) 2010 - 2016 Radek Dostál
+ * @copyright Copyright (c) 2010 - 2017 Radek Dostál
  * @license   GNU Lesser General Public License
  * @link      http://www.radekdostal.cz
  */
